2736cb2e53034b9f6aa941ddfb8c77d49210fbb1
박우승 <102569953+somnium-Esthetics@users.noreply.github.com>
2025-11-24 12:03:49 +0900

    assignments

--- ch06lab2_가장긴문자열.cpp
+++ ch06lab2_가장긴문자열.cpp
@@ -1,40 +1,46 @@
 //ch06lab2_가장긴문자열.cpp
+//문자열 n개를 입력 받고 가장 긴 문자열을 반환하는 코드
 #define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
-#include <string>
 #define SIZE 100
 using namespace std;
 
+
+
 int main() {
-	char str[SIZE];
-	char* result = NULL;
+	int num;
+	cout << "단어의 개수를 입력하세요 :";
+	cin >> num;
 
-	while (true) {
-		cout << "문자열을 입력하세요." << endl;
-		cin >> str;
-		//배열 하나 만들어서 넣고 긴거를 비교해서 넣고 지우기
-		
-		if (result == NULL) {
-			result = new char[strlen(str) + 1];
-			strcpy(result, str);// rseult는 첫값
-		}
-		else {//두번째 부터
-			if (strlen(result) < strlen(str)) {
-				char* temp = new char[strlen(str)];
-				strcpy(temp, str)// 여기서부터 다시해봇기
-			}
-			else {
+	char** words = new char*[num];// WORD라는 포인터는 num개의 char 포인터를 접근하게 해주는 포인터
+	for (int i = 0; i < num; i++) {
+		words[i] = new char[SIZE];//메모리 할당
+		cout << "단어를 입력하세요 :";
+		cin >> words[i];
+		cout << endl;
+		cout << "=======================" << endl;
 
-			}
-		}
-
-		if (strcmp(str, "exit") == 0) {
-			break;
-		}
-
-	
 	}
 
-	return 0;
 
+
+	char ans[SIZE] = "default";
+	for (int i = 0; i < num; i++) {
+		if (strlen(ans) == 7) {
+			strcpy(ans, words[i]);
+		}
+		else {
+			if (strlen(ans) < strlen(words[i])) {
+				strcpy(ans, words[i]);
+			}
+		}
+
+
+	}
+	cout << "가장 긴 문자열은 " << ans << "입니다" << endl;
+
+	delete[] words;
+
+	return 0;
 }
+
