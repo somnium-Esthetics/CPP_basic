c61825905774822745a6d056b953d2887ba367c2
박우승 <102569953+somnium-Esthetics@users.noreply.github.com>
2025-11-29 18:14:23 +0900

    assignment_4_2

--- ch07lab5_rsp.cpp
+++ ch07lab5_rsp.cpp
@@ -1,33 +1,26 @@
 //ch07lab5_rsp.cpp
+//ch07lab5_rsp.cpp
 #include<iostream>
 #define GAME_CNT 3
 #define ROCK 1
 #define SCISSOR 2
 #define PAPER 3
-/*
-* c u
-1r 2r
-2s 3s
-3p 4p
 
-1 비김
-2나 -1 짐
-0나 3 이김
-user
-*/
+
 using namespace std;
 //enum 사용
 class RSP {
 public:
-	void start_game();
+	void start_game();// 게임 시작
 private:
-	int user=0;
+	int user = 0;
 	int com;
 	int count[3] = { 0 };// 승리 비김 짐user 
-	
+
 	void getCom();// 컴퓨터 값 입력
-	int getUser();// 유저의 값 입력
+	int getUser( );// 유저의 값 입력
 	void checkUser_(); //가위 바위 보 진행
+	void replay();
 
 
 };
@@ -43,8 +36,8 @@ int main() {
 
 void RSP::start_game()
 {
-	getCom();
-	
+	getCom();// 컴 값 초기화
+
 	checkUser_();
 
 }
@@ -59,7 +52,7 @@ void RSP::getCom()
 int RSP::getUser()
 {
 	int user_choose;
-	cout << "바위(2), 가위(3), 보(4) 를 입력하세요 : " << endl;
+	cout << "바위(1), 가위(2), 보(3) 를 입력하세요 : " << endl;
 	cin >> user_choose;
 	return user_choose;
 }
@@ -67,23 +60,45 @@ int RSP::getUser()
 void RSP::checkUser_()
 
 {
-	string rsp[] = { "", "가위","바위","보" };//사용하기
-	user=getUser();
+	cout << "===================================" << endl;
+	string rsp[] = { "", "바위","가위","보" };//사용하기
+	user = getUser();
 	cout << "com :" << com << "   you: " << user << endl;
-	if (user - com == 1) {//비긴 경우
+	if (user==com) {//비긴 경우
+		cout << "com: " << rsp[com] << endl;
+		cout << "user: " << rsp[user] << endl;
 		cout << "비겼습니다." << endl;
 		count[1]++;
 	}
-	else if (user - com == 2&& -1) {// 진 경우
+	else if ((user ==ROCK&& com==PAPER)||(user==SCISSOR&&com ==ROCK)||(user==PAPER&&com==SCISSOR)) {// 진 경우
+		cout << "com: " << rsp[com] << endl;
+		cout << "user: " << rsp[user] << endl;
 		cout << "패배했습니다." << endl;
 		count[2]++;
 
 	}
-	else if (user - com == 0 && 3) {//이긴 경우
+	else if( (user==ROCK&&com==SCISSOR)||(user==SCISSOR&&com==PAPER)||(user==PAPER&&com==ROCK)) {//이긴 경우
+		cout << "com: " << rsp[com] << endl;
+		cout << "user: " << rsp[user ] << endl;
 		cout << "승리했습니다." << endl;
 		count[0]++;
 	}
+	cout << "다시 하시겠습니까? (O,X)" << endl;
+	replay();
 }
 
-//string rsp[] = {"", "가위","바위","보"}
-//rsp[com]
+void RSP::replay()
+{
+	char rp;
+	cin >> rp;
+	if (rp == 'o' || rp == 'O') {
+		checkUser_();
+	}
+	else {
+		cout << "게임이 끝났습니다." << endl;
+		cout << "결과를 산출합니다." << endl;
+		cout << "승리 : " << count[0] << "회" << endl;
+		cout << "비김 : " << count[1] << "회" << endl;
+		cout << "패배 : " << count[2] << "회" << endl;
+	}
+}
